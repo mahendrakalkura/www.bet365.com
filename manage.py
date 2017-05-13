62e58b567ddf87765959e3c1a374c3ed6c6259f7
Mahendra Kalkura <mahendra@kalkura.com>
2017-05-14 01:17:51 +0530

    manage.py

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ def trace(function):
 
 class WebSockets():
 
-    URL = 'wss://premws-pt2.365lpodds.com/zap'
+    URL = 'wss://premws-pt2.365lpodds.com/zap/'
 
     DELIMITERS_MESSAGE = '\x00'
     DELIMITERS_RECORD = '\x01'
@@ -51,22 +51,36 @@ class WebSockets():
             self.URL,
             on_open=self.on_open,
             on_close=self.on_close,
+            on_data=self.on_data,
             on_message=self.on_message,
             on_error=self.on_error,
+            header={
+                'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
+                'Sec-WebSocket-Protocol': 'zap-protocol-v1',
+                'Sec-WebSocket-Version': '13',
+            },
         )
         self.connection.run_forever()
 
     @trace
     def on_open(self, _):
-        self.log('Open', None)
-        topic = '__time,S_%s' % self.session_id
-        message = '\x23\x01\x17%s%s\x00' % (chr(len(topic) + 1), topic)
-        self.log(message)
-        self._connection.send(bytearray(message, 'utf-8'))
+        print('Open')
+        message = '#\x03P\x01__time,S_%s' % self.session_id
+        print(repr(message))
+        self.connection.send(bytearray(message, 'utf-8'))
 
     @trace
     def on_close(self, _):
-        self.log('Close', None)
+        print('Close')
+
+    @trace
+    def on_data(self, a, b, c):
+        a = bytearray(a, 'utf-8')
+        print(repr(a))
+        b = bytearray(b, 'utf-8')
+        print(repr(b))
+        c = bytearray(c, 'utf-8')
+        print(repr(c))
 
     @trace
     def on_message(self, _, message):
@@ -75,24 +89,7 @@ class WebSockets():
 
     @trace
     def on_error(self, _, error):
-        self.log('Error', error)
-
-    @trace
-    def log(self, prefix, suffix):
-        if prefix and suffix:
-            prefix = repr(prefix)
-            print('[+]', prefix, ':', suffix)
-            return
-        if prefix:
-            print('[+]', prefix)
-            return
-
-    @trace
-    def send(self, type, headers, data):
-        message = [type, self._get_headers(headers)]
-        if data:
-            message.append(data)
-        self._connection.send(bytearray(''.join(message), 'utf-8'))
+        print('Error', repr(error))
 
 
 @trace
