624a5dc5fcaed85f3e766c7ab4bcd8a5f653b30a
Mahendra Kalkura <mahendra@kalkura.com>
2017-05-15 10:35:20 +0530

    manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from sys import argv
 from traceback import print_exc
 
 from requests import request
-from websocket import WebSocketApp
+from ws4py.client.threadedclient import WebSocketClient
 
 
 def trace(function):
@@ -15,87 +15,107 @@ def trace(function):
     return wrap
 
 
-class WebSockets():
+class WebSockets(WebSocketClient):
 
-    URL = 'wss://premws-pt2.365lpodds.com/zap/'
+    URL = u'wss://premws-pt1.365lpodds.com/zap/'
 
-    DELIMITERS_MESSAGE = '\x00'
-    DELIMITERS_RECORD = '\x01'
-    DELIMITERS_FIELD = '\x02'
+    HEADERS = [
+        (u'Sec-WebSocket-Extensions', u'permessage-deflate;client_max_window_bits'),
+        (u'Sec-WebSocket-Protocol', u'zap-protocol-v1'),
+        (u'Sec-WebSocket-Version', u'13'),
+    ]
 
-    TYPES_CLIENT_ID = '4'
-    TYPES_TOPIC_LOAD_MESSAGE = '\x14'
-    TYPES_DELTA_MESSAGE = '\x15'
-    TYPES_SUBSCRIBE = '\x16'
-    TYPES_PING_CLIENT = '\x19'
+    DELIMITERS_MESSAGE = u'\x08'
+    DELIMITERS_RECORD = u'\x01'
+    DELIMITERS_FIELD = u'\x02'
+
+    TYPES_TOPIC_LOAD_MESSAGE = u'\x14'
+    TYPES_DELTA_MESSAGE = u'\x15'
+    TYPES_SUBSCRIBE = u'\x16'
+    TYPES_PING_CLIENT = u'\x19'
+
+    TOPICS = [
+        u'CONFIG_1_3',
+        u'OVInPlay_1_3',
+        u'Media_L1_Z3',
+        u'XL_L1_Z3_C1_W5',
+    ]
 
     @trace
     def __init__(self, id):
         self.id = id
-        self.session_id = self.get_session_id()
+        super(WebSockets, self).__init__(self.URL, headers=self.HEADERS)
+        self.session_id = get_session_id()
 
     @trace
-    def get_session_id(self):
-        response = None
-        try:
-            response = request(method='GET', url='https://www.bet365.com/?#/AS/B1/')
-        except Exception:
-            pass
-        if not response:
+    def connect(self):
+        if not self.session_id:
+            print('Invalid Session ID')
             return
-        return response.cookies['pstk']
+        super(WebSockets, self).connect()
 
     @trace
-    def open(self):
-        self.connection = WebSocketApp(
-            self.URL,
-            on_open=self.on_open,
-            on_close=self.on_close,
-            on_data=self.on_data,
-            on_message=self.on_message,
-            on_error=self.on_error,
-            header={
-                'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
-                'Sec-WebSocket-Protocol': 'zap-protocol-v1',
-                'Sec-WebSocket-Version': '13',
-            },
-        )
-        self.connection.run_forever()
+    def opened(self):
+        print('Opened')
+        message = u'#\x03P\x01__time,S_%s\x00' % self.session_id
+        self.send(message)
 
     @trace
-    def on_open(self, _):
-        print('Open')
-        message = '#\x03P\x01__time,S_%s' % self.session_id
-        print(repr(message))
-        self.connection.send(bytearray(message, 'utf-8'))
+    def closed(self, code, reason=None):
+        print('Closed', code, reason)
 
     @trace
-    def on_close(self, _):
-        print('Close')
+    def received_message(self, message):
+        message = unicode(message)
+        print('<=', repr(message))
+        message = self.decode(message)
+        if message[0][0] == u'100':
+            message = u'\x16\x00' + u','.join(self.TOPICS) + u'\x01\x00'
+            self.send(message)
+            return
+        if message[0][0] == u'\x14EMPTY':
+            message = u'\x16\x006V64507930C1A_1_3\x01\x00'
+            self.send(message)
+            message = u'\x16\x0015332427912M1_1_3\x01\x00'
+            self.send(message)
+            return
 
     @trace
-    def on_data(self, a, b, c):
-        a = bytearray(a, 'utf-8')
-        print(repr(a))
-        b = bytearray(b, 'utf-8')
-        print(repr(b))
-        c = bytearray(c, 'utf-8')
-        print(repr(c))
-
-    @trace
-    def on_message(self, _, message):
+    def send(self, message):
+        print('=>', repr(message))
         message = bytearray(message, 'utf-8')
-        print(repr(message))
+        super(WebSockets, self).send(message)
 
     @trace
-    def on_error(self, _, error):
-        print('Error', repr(error))
+    def decode(self, message):
+        message = message.strip(self.DELIMITERS_MESSAGE)
+        message = message.split(self.DELIMITERS_RECORD)
+        message = [m.split(self.DELIMITERS_FIELD) for m in message]
+        message = [[item for item in items if item] for items in message]
+        message = [m for m in message if m]
+        return message
+
+
+@trace
+def get_session_id():
+    response = None
+    try:
+        response = request(method='GET', url='https://www.bet365.com/?#/AS/B1/')
+    except Exception:
+        pass
+    if not response:
+        return
+    return response.cookies['pstk']
 
 
 @trace
 def main(options):
-    web_sockets = WebSockets(options[1])
-    web_sockets.open()
+    try:
+        web_sockets = WebSockets(options[1])
+        web_sockets.connect()
+        web_sockets.run_forever()
+    except KeyboardInterrupt:
+        web_sockets.close()
 
 
 if __name__ == '__main__':
